06c5272f1d838e102318fc73a833bc40dbb283b3
DEVISRIARJUN <devisriarjun268@gmail.com>
2026-08-19 23:51:02 +0530

    updated

--- lists/miss_first_four_num.py
+++ lists/miss_first_four_num.py
@@ -3,10 +3,11 @@
 l = [6,81,7,15,11,37,23,13,27,16]
 k = min(l) + 1
 c = 0
-while True:
+while (True):
     if k not in l:
         print(k,end=" ")
         c = c + 1
-    if c == 4:
-        break
+        if c == 4:
+            break
     k = k + 1
+
